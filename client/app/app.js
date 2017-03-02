e0c19c4b97e03c05116f6db3a16f6ae7cd5249f9
bi912319@jp.ibm.com <yxmyj394>
2017-03-02 19:41:44 +0900

    ツールバー読み込み機能を追加

--- client/app/app.js
+++ client/app/app.js
@@ -21,4 +21,21 @@ angular.module('nitoiotweb11App', [
       });
 
     $locationProvider.html5Mode(true);
+  })
+  .directive('navitop', function () {
+      return {
+          priority: 0,
+          templateUrl: '../components/navbar/navbar.html',
+          replace: false,
+          transclude: false,
+          restrict: 'E',
+          scope: false,
+          controller: 'NavbarCtrl'
+          // ['$scope', '$route', '$location', function ($scope, $route, $location) {
+          //     $scope.$on('$routeChangeSuccess', function () {
+          //         var urlFlagments = $location.$$path.split("/");
+          //         $scope.currentPage = urlFlagments[1];
+          //     });
+          // }]
+      };
   });
