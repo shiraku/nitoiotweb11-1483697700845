e0fbf011aa9fc5cdfca5151d59be88fc39dda2a9
bi912319@jp.ibm.com <yxmyj394>
2017-03-14 20:27:09 +0900

    GoogleChart読み込み様ために修正

--- client/app/app.js
+++ client/app/app.js
@@ -10,7 +10,9 @@ angular.module('nitoiotweb11App', [
   'ngAria',
   'ngMessages',
   'ui.bootstrap',
-  'uiGmapgoogle-maps'
+  'uiGmapgoogle-maps',
+  'googlechart'
+  // "googlechart-docs"
   // 'material.svgAssetsCache'
 
 ])
@@ -35,11 +37,20 @@ angular.module('nitoiotweb11App', [
           restrict: 'E',
           scope: false,
           controller: 'NavbarCtrl'
-          // ['$scope', '$route', '$location', function ($scope, $route, $location) {
-          //     $scope.$on('$routeChangeSuccess', function () {
-          //         var urlFlagments = $location.$$path.split("/");
-          //         $scope.currentPage = urlFlagments[1];
-          //     });
-          // }]
+      };
+  })
+  .directive('editIcon', function () {
+      return {
+          priority: 0,
+          template: '<svg xmlns="http://www.w3.org/2000/svg" fill="#000000" height="24" viewBox="0 0 24 24" width="24">'
+                    +'<path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>'
+                    +'<path d="M0 0h24v24H0z" fill="none"/>'
+                    +'</svg>',
+          replace: false,
+          transclude: false,
+          restrict: 'E',
+          scope: false//,
+    //       controller: ['$scope', function($scope){ // Controller定義
+    // }]
       };
   });
