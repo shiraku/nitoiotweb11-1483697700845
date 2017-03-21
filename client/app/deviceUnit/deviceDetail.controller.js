555acbe934a21fddd55a4a68eb7276e53d65a398
bi912319@jp.ibm.com <yxmyj394>
2017-03-21 23:48:28 +0900

    デバイス詳細画面　APIからの取得してきたデータを画面に表示するように変更

--- client/app/deviceUnit/deviceDetail.controller.js
+++ client/app/deviceUnit/deviceDetail.controller.js
@@ -10,164 +10,93 @@
       .then(function successCallback(response) {
         console.log("/api/device_detail/00000 successfully");
         console.log(response);
+
+        var obj = response.data;
+        var obj2;
+        if(obj.earthquakeCurrentData.datas){
+        obj2 = obj.earthquakeCurrentData.datas;
+      }else{
+        obj2 = obj.thunderCurrentData.datas
+      }
+
+        //ヘッダータイトル
+        $scope.navtitle = obj.deviceName;
+
+        if(obj2.commercialBlackout){
+          obj2.commercialBlackout = "あり"
+        }else{
+          obj2.commercialBlackout = "なし"
+        }
+
+        if(obj2.equipmentAbnormality){
+          obj2.equipmentAbnormality = "あり"
+        }else{
+          obj2.equipmentAbnormality = "なし"
+        }
+
+        if(obj2.leakage){
+          obj2.leakage = "あり"
+        }else{
+          obj2.leakage = "なし"
+        }
+
+        if(obj2.slope){
+          obj2.slope = "あり（"+obj2.slope+"°）";
+        }else{
+          obj2.slope = "なし"
+        }
+
+        $scope.deviceDetail =
+        {
+             deviceId       :obj._id,
+             deviceName     :obj.deviceName,
+             responsiblePerson :obj.responsiblePerson,
+             telNo          :obj.telNo,
+             address        :obj.address,
+             memo           :obj.memo
+       };
+
+       //地震の最新データある場合のみObjectにpush
+       if(obj.earthquakeCurrentData){
+         $scope.deviceDetail.earthquakeCurrentData = obj.earthquakeCurrentData;
+       }
+       //雷の最新データある場合のみObjectにpush
+       if(obj.thunderCurrentData){
+         $scope.deviceDetail.thunderCurrentData = obj.thunderCurrentData;
+       }
+
+
       }, function errorCallback(response) {
         console.error("error in /api/device_detail/00000");
       });
-      
-      
+
+
       //地震履歴情報
       $http.get('/api/device_history_eq/00000')
       .then(function successCallback(response) {
         console.log("/api/device_history_eq/00000 successfully");
         console.log(response);
+
+        $scope.earthquakeHistoryList = response.data.data;
+        $scope.thunderHistoryList = response.data.data;
+
       }, function errorCallback(response) {
         console.error("error in /api/device_history_eq/00000");
       });
-      
-      
-      //TODO APIでデータ取得をする
-      $scope.deviceDetail = [
-        {
-          "deviceId":"001",//デバイスID(非表示)
-          "deviceName":"デバイス１",//デバイス名
-          'responsiblePerson':'小林　武',//責任者名
-          'telNo':'000-0000-0000',//連絡先
-          'address':'愛知県長久手市蟹原2201番地',//住所
-          'memo':'本社横の倉庫',//メモ
-          'earthquakeCurrentData':[{//地震の最新ステータスデータ
-                     "date_id":"20170216110735",
-                     'seismicIntensity':'5.4',//地震
-                     'slope':'倒壊',//傾き
-                     'commercialBlackout':'あり',//商用停電
-                     'equipmentAbnormality':'なし',//機器異常
-                     'commentList':[//コメント・連絡事項
-                       {
-                         'comment':'建物に破損あり',
-                         'commentDate':'2017/01/15 10:15',
-                       },
-                       {
-                         'comment':'業務を一時停止',
-                         'commentDate':'2017/01/15 10:15',
-                       }],
-                       'mediaNewsletter':[
-                         {
-                           'japanMeteorologicalAgency':'4',//メディア速報　震度
-                           'longPeriodSeismicActivityClass':'3'//メディア速報長周期地震動階級
-                       }
-                     ]
-               }],
-            'thunderCurrentData':[
-                 {
-                      "date_id":"20170216110735",
-                      'power':'5.4',//雷
-                      'leakage':'なし',//漏電
-                      'commercialBlackout':'あり',//商用停電
-                      'equipmentAbnormality':'なし',//機器異常
-                      'commentList':[//コメント連絡事項
-                        {
-                          'comment':'建物に破損あり',
-                          'commentDate':'2017/01/15 10:15',
-                        },
-                        {
-                          'comment':'業務を一時停止',
-                          'commentDate':'2017/01/15 10:15',
-                        }],
-                      'mediaNewsletter':[{
-                          'japanMeteorologicalAgency':'4',//メディア速報　震度
-                          'longPeriodSeismicActivityClass':'3'//メディア速報長周期地震動階級
-                      }]
-
-                }
-            ],
-            'earthquakeHistoryList':[//地震の履歴データ
-              {
-                //"device_id":"00000",
-                "data":[
-                  {
-                  "date_id":"20170216110735",
-                  "date":"2017/02/16 11:07:35",
-                  "datas":
-                  [{"name":"震度",
-                    "value":6,
-                    "values":"6_u"
-                  }]},
-                  {"date_id":"20170216110254","date":"2017/02/16 11:02:54","datas":[{"name":"震度","value":6,"values":"6_u"}]},
-                  {"date_id":"20170216105831","date":"2017/02/16 10:58:31","datas":[{"name":"震度","value":6,"values":"6_u"}]},
-                  {"date_id":"20170216105553","date":"2017/02/16 10:55:53","datas":[{"name":"震度","value":6,"values":"6_u"}]},
-                  {"date_id":"20170215182055","date":"2017/02/15 18:20:55","datas":[{"name":"震度","value":6,"values":"6_u"}]},
-                  {"date_id":"20170215180448","date":"2017/02/15 18:04:48","datas":[{"name":"震度","value":6,"values":"6_u"}]},
-                  {"date_id":"20170215174601","date":"2017/02/15 17:46:01","datas":[{"name":"震度","value":6,"values":"6_u"}]},
-                  {"date_id":"20170215172801","date":"2017/02/15 17:28:01","datas":[{"name":"震度","value":6,"values":"6_u"}]},
-                  {"date_id":"20170215163422","date":"2017/02/15 16:34:22","datas":[{"name":"震度","value":6,"values":"6_u"}]},
-                  {"date_id":"20170214194843","date":"2017/02/14 19:48:43","datas":[{"name":"震度","value":6.5,"values":"7"}]}
-                ]}
-            ],
-            'thunderHistoryList':[//雷の履歴データ
-              {
-                // "device_id":"00000",
-                "data":[
-                  {"date_id":"20161025160205",
-                  "date":"2016/10/25 16:02:05",
-                  "datas":[{
-                    "value":2,
-                    "name":"雷レベル"
-                  }]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20161025153619","date":"2016/10/25 15:36:19","datas":[{"value":2,"name":"雷レベル"}]},
-                  {"date_id":"20160830030221","date":"2016/08/30 03:02:21","datas":[{"value":3,"name":"雷レベル"}]}]}
-
-            ]
-      }];
-
-        //ヘッダータイトル
-        $scope.navtitle=$scope.deviceDetail[0].deviceName;
 
 
+      //TODO さらに見るボタンが動かないので、修正する。
         $scope.earthquakeHistoryViews = 10; // 地震情報表示する件数(初期表示件数を指定)
         $scope.thunderHistoryViews = 10; //　雷情報表示する件数(初期表示件数を指定)
         $scope.nextView = 10; // 地震&雷情報もっと見るをクリックしたときに表示する数
 
-        // 地震情報もっと見るクリック時
+        // 地震情報さらに見るクリック時
         $scope.earthquakeHistoryMoreClick = function () {
             $scope.moreCount++;
             $scope.earthquakeHistoryViews = $scope.earthquakeHistoryViews + $scope.nextView;
         };
 
-        // 雷情報もっと見るクリック時
+        // 雷情報さらに見るクリック時
         $scope.thunderHistoryMoreClick = function () {
             $scope.moreCount++;
             $scope.thunderHistoryViews = $scope.thunderHistoryViews + $scope.nextView;
