3507a07d2c57d18c46d7a4c851423d4e95e6d8a6
shiraku <shiraku@jp.ibm.com>
2017-04-20 23:55:14 +0900

    参照cloudantを01から11に変更

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -5,9 +5,9 @@
 var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
 module.exports = {
 
-   CHOST: vcapServices.cloudantNoSQLDB[0].credentials.host,
-   CPORT: vcapServices.cloudantNoSQLDB[0].credentials.port,
-   CUSER: vcapServices.cloudantNoSQLDB[0].credentials.username,
-   CPASSWORD: vcapServices.cloudantNoSQLDB[0].credentials.password,
-   CURL: vcapServices.cloudantNoSQLDB[0].credentials.url
+   CHOST: vcapServices.cloudantNoSQLDB[1].credentials.host,
+   CPORT: vcapServices.cloudantNoSQLDB[1].credentials.port,
+   CUSER: vcapServices.cloudantNoSQLDB[1].credentials.username,
+   CPASSWORD: vcapServices.cloudantNoSQLDB[1].credentials.password,
+   CURL: vcapServices.cloudantNoSQLDB[1].credentials.url
 };
