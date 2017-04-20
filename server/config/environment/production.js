c55089c9ecb6dc73fddf6884d171b7021777ca3d
shiraku <shiraku@jp.ibm.com>
2017-04-20 18:47:01 +0900

    サーバー起動コマンド、環境設定更新

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -8,6 +8,6 @@ module.exports = {
    CHOST: vcapServices.cloudantNoSQLDB[0].credentials.host,
    CPORT: vcapServices.cloudantNoSQLDB[0].credentials.port,
    CUSER: vcapServices.cloudantNoSQLDB[0].credentials.username,
-   CPASSWORD: vcapServices.cloudantNoSQLDB[0].credentials.password;,
+   CPASSWORD: vcapServices.cloudantNoSQLDB[0].credentials.password,
    CURL: vcapServices.cloudantNoSQLDB[0].credentials.url
 };
