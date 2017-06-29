6edb3ae395e206b112658d49490a802c3af2f920
shiraku <shiraku@jp.ibm.com>
2017-06-29 14:25:16 +0900

    Package.json nodeとnpmのバージョン設定削除 Cloudantの接続先を11に変更

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -4,10 +4,17 @@
 // =================================
 var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
 module.exports = {
-
+  //*/ //nitoiotweb11-cloudantNoSQLDB
+   CHOST: vcapServices.cloudantNoSQLDB[0].credentials.host,
+   CPORT: vcapServices.cloudantNoSQLDB[0].credentials.port,
+   CUSER: vcapServices.cloudantNoSQLDB[0].credentials.username,
+   CPASSWORD: vcapServices.cloudantNoSQLDB[0].credentials.password,
+   CURL: vcapServices.cloudantNoSQLDB[0].credentials.url
+  /*/ //nitoiotweb01-cloudantNoSQLDB
    CHOST: vcapServices.cloudantNoSQLDB[1].credentials.host,
    CPORT: vcapServices.cloudantNoSQLDB[1].credentials.port,
    CUSER: vcapServices.cloudantNoSQLDB[1].credentials.username,
    CPASSWORD: vcapServices.cloudantNoSQLDB[1].credentials.password,
    CURL: vcapServices.cloudantNoSQLDB[1].credentials.url
+  /*/
 };
